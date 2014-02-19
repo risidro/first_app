edafa788965878c5f89765a38138d700ac5f1611
risidro <risidro120478@gmail.com>
2014-02-19 09:06:55 -0500

    Roberto-index.html update

---
+++ rails_projects/demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'e2bff6414537b3f6977eb903006dc0ff49c95c8ec980d67587f386fc649f471253824c971cf7c692de98a0108e2844fdcc8bd50180670aa17a5e08401a4f48ae'
